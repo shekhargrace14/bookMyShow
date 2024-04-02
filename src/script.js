205a9c85361596457295bd43280ea23d9e0d0998
shekhargrace14 <82882207+shekhargrace14@users.noreply.github.com>
2024-04-02 12:24:17 +0000

    its working hurry

--- src/script.js
+++ src/script.js
@@ -4,11 +4,45 @@ fetch("./data/hero.json")
 .then((resolve)=>resolve.json())
 .then((data)=>{
     console.log(data)
-    data.map((item)=>{
+    // slice 4 has stopped the showing of the hero images 
+    let newData = data.slice(4)
+    newData.map((item)=>{
         track.insertAdjacentHTML("beforeend",`
-        <div class="column w-[100%]">
-            <img w-[100%] src="${item.img}" alt="">
+        <div class="column w-[100%] flex justify-center">
+            <img w-[100%]  src="${item.img}" alt="">
         </div>
         `)
     })
 })
+
+
+
+// movies slider starts here 
+async function moviesData(){
+    let resolve = await fetch("https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1&api_key=185c7d1fa7ff15b5023522fae491e666");
+    let data = await resolve.json();
+    console.log(data)
+    showMoviesData(data)
+    return data
+}
+moviesData()
+
+function showMoviesData(data){
+    let moviesRow = document.querySelector(".moviesRow");
+
+    data.results.map((items)=>{
+        moviesRow.insertAdjacentHTML("beforeend",`
+        <div class="column card">
+          <figure>
+            <img class="rounded" src=" https://image.tmdb.org/t/p/w500${items.poster_path}" alt="">
+          </figure>
+          <div class="info">
+            <h4 class="font-semibold">Kung Fu Panda 4</h4>
+            <p class="genre">Action/Adventure/Animation/Comedy</p>
+          </div>
+        </div>
+
+        `)
+    })
+}
+// movies slider ends  here 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      colors: {
-        clifford: '#da373d',
-        colorPrimary: "#f84464",
-        colorPrimaryDark: "#B91A37",
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
