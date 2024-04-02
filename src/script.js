07b27644b2779611db054c1ba023ab80656d8f0d
shekhargrace14 <82882207+shekhargrace14@users.noreply.github.com>
2024-04-02 09:38:22 +0000

    cli installed

---
+++ src/script.js
@@ -0,0 +1,14 @@
+let track = document.querySelector(".track")
+
+fetch("./data/hero.json")
+.then((resolve)=>resolve.json())
+.then((data)=>{
+    console.log(data)
+    data.map((item)=>{
+        track.insertAdjacentHTML("beforeend",`
+        <div class="column w-[100%]">
+            <img w-[100%] src="${item.img}" alt="">
+        </div>
+        `)
+    })
+})
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+        colorPrimary: "#f84464",
+        colorPrimaryDark: "#B91A37",
+      }
+    },
+  },
+  plugins: [],
+}
+
